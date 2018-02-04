335aa26e293a910558ba31cc77d57d32b6505fbf
Shan Gardezi <shangardezi@me.com>
2018-02-04 15:54:04 +0000

    spec for adding valid products to basket

--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -1,12 +1,19 @@
 require 'checkout'
 
 RSpec.describe Checkout do
+
   subject { described_class.new promo_rules }
 
   describe '#scan' do
     let(:invalid_product_error) { Checkout::PRODUCT_NOT_FOUND_ERROR }
+
     it "raises a product not found error if product doesn't exist" do
       expect{ subject.scan('foo_bar') }.to raise_error invalid_product_error
+    end
+
+    it 'adds valid products to the basket' do
+      expect(subject).to receive(:add_to_basket).with('003')
+      subject.scan('003')
     end
   end
 
