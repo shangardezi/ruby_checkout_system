12fa5c765a4db78866bc3a64ed9a8315ce65a7b3
Shan Gardezi <shangardezi@me.com>
2018-02-04 20:56:27 +0000

    spec for entire feature

---
+++ spec/feature_spec.rb
@@ -0,0 +1,30 @@
+require 'checkout'
+
+RSpec.describe 'Feature' do
+  let(:checkout){ Checkout.new(promo_rules) }
+
+  subject { checkout.total }
+
+  it 'returns the correct basket amount based on value rules' do
+    checkout.scan('003')
+    checkout.scan('003')
+    checkout.scan('003')
+    expect(subject).to eq 59.85
+  end
+
+  it 'eturns the correct basket amount based on volume rules' do
+    checkout.scan('001')
+    checkout.scan('002')
+    checkout.scan('001')
+    expect(subject).to eq 55.80
+  end
+
+  it 'eturns the correct basket amount based on value and volume rules' do
+    checkout.scan('001')
+    checkout.scan('003')
+    checkout.scan('003')
+    checkout.scan('002')
+    expect(subject).to eq(84.74)
+  end
+
+end
