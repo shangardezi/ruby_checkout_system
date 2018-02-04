42e65b9a148ebfbd2b40f821471a47a80b512f0a
Shan Gardezi <shangardezi@me.com>
2018-02-04 15:31:08 +0000

    create checkout file with products (refactor into products into json file?)

---
+++ lib/checkout.rb
@@ -0,0 +1,17 @@
+class Checkout
+  PRODUCTS =  { '001': { name: 'Very Cheap Chair', price: 9.25 },
+                '002': { name: 'Little table', price: 45.00 },
+                '003': { name: 'Funkly light', price: 19.95 } }
+  
+  def initialize promo_rules
+
+  end
+
+  def scan
+
+  end
+
+  def total
+
+  end
+end
