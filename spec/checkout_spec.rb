74cd9d85fee7d7b83270f2b2c229cbfd56d99ef6
Shan Gardezi <shangardezi@me.com>
2018-02-04 15:48:49 +0000

    invalid product spec

--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -1,9 +1,34 @@
-RSpec.describe Checkout do
-  describe '#scan' do
+require 'checkout'
 
+RSpec.describe Checkout do
+  subject { described_class.new promo_rules }
+
+  describe '#scan' do
+    let(:invalid_product_error) { Checkout::PRODUCT_NOT_FOUND_ERROR }
+    it "raises a product not found error if product doesn't exist" do
+      expect(subject.scan('foo_bar') ).to raise_error invalid_product_error
+    end
   end
 
   describe '#total' do
-
   end
+
+  private
+
+    def promo_rules
+      {
+        value_rules: [
+           {
+             minimum_value: 60,
+             discount_decimal: 0.1
+           }
+                     ],
+        volume_rules: {
+          "001": {
+            minimum_volume_required: 2,
+            discounted_price: 8.50
+          }
+        }
+      }
+    end
 end
