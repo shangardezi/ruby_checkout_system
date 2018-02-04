306ec1a0fde3808e0428192c1b5a58f76c3a64c8
Shan Gardezi <shangardezi@me.com>
2018-02-04 20:38:42 +0000

    spec for calculator

---
+++ spec/calculator_spec.rb
@@ -0,0 +1,18 @@
+require 'calculator'
+
+RSpec.describe Calculator do
+  let(:basket) { { '002': 3 } }
+
+  subject { described_class.new(promo_rules, dummy_products) }
+
+  describe '#total' do
+
+    before do
+      allow(subject).to receive(:get_discounted_price).and_return false
+    end
+    
+    it 'returns correct total for basket' do
+      expect(subject.total(basket)).to eq 121.5
+    end
+  end
+end
